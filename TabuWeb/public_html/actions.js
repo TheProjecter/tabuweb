78967974ed9d6e7d9f16dd066851cd47c67f780b
nathaniel.lacerda <nathaniel.lacerda@8d21030a-e83b-8828-8c38-daa317ffc0a6>
2012-10-17 11:35:43 +0000

    Introdução do javascript: submissão no nome de usuário

---
+++ TabuWeb/public_html/actions.js
@@ -0,0 +1,7 @@
+
+/*
+ * Função que substitui o formulário de sumbissão do nome de usuário pelo próprio nome recebido.
+ */
+function receiveUserName(name) {
+	document.getElementById("submit_name").innerHTML = name;
+}

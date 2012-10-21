0d2f3326401856b00061bd07cf875146a5062873
kazuya.yamazaki <kazuya.yamazaki@8d21030a-e83b-8828-8c38-daa317ffc0a6>
2012-10-21 13:59:02 +0000

    fiz animações

--- TabuWeb/public_html/actions.js
+++ TabuWeb/public_html/actions.js
@@ -1,3 +1,4 @@
+var jogoPausado = 1;
 /*
  * Função que substitui o formulário de sumbissão do nome de usuário pelo próprio nome recebido.
  */
@@ -13,9 +14,11 @@ function startStopGame() {
 	if (option == "START") {
 		document.getElementById("control_button").innerHTML = "STOP";
 		alert("Let's start the game!");
+		jogoPausado = 0;
 	} else if (option == "STOP") {
 		document.getElementById("control_button").innerHTML = "START";
 		alert("Bye bye");
+		jogoPausado = 1;
 	} else {
 		alert("Opção inválida: " + option);
 	}
@@ -30,7 +33,7 @@ function hideConfigurations() {
 	document.getElementById("configuration_popup").style.visibility="hidden";
 }
 
-/* Mexe zumbie */
+/*Mexe zumbie */
 var d_left, d_top, movimento_left, movimento_top, pos_x, pos_y;
 
 // Quantos px andará cada intervalo
@@ -41,51 +44,66 @@ movimento_top = parseInt(Math.random()*3 + 1);
 d_left = parseInt(Math.random()*2);
 d_top = parseInt(Math.random()*2);
 
+/* Opções */
+var dificuldade = 1, soma = 1, subt = 1, mult = 1, divi = 1, offline = 1, cenario = 1;
+
+function mudaDificuldade(valor){dificuldade = valor;}
+function mudaSoma(){if(soma == 1) soma = 0; else soma = 1;}
+function mudaSubt(){if(subt == 1) subt = 0; else subt = 1;}
+function mudaMult(){if(mult == 1) mult = 0; else mult = 1;}
+function mudaDivi(){if(divi == 1) divi = 0; else divi = 1;}
+function mudaOffline(valor){offline = valor;}
+function mudaCenario(valor){cenario = valor;}
+
+var num = 0;
+
 $(document).ready(function(){
-	var pos_x = parseInt($('#field').width() / 2 - $('#zumbie_1').width() / 2 );
-	var pos_y = parseInt($('#field').height() / 2 - $('#zumbie_1').height() / 2 );
+	/*Pega posição*/
+	var pos_x1 = 10, pos_y1 = 50, pos_x2 = 10, pos_y2 = 100;
+	alert(num);
+	num++;
 	
+	/*Define Limites*/	
 	limite_left = 20;
 	limite_right = $('#field').width() - $('#zumbie_1').width() - 20;
-	limite_top = 0;
-	limite_bottom = $('#field').height() - $('#zumbie_1').height();
 	
-	playButton = $('#playgame');
-	playButton.click(function(){
-		
-		window.setTimeout(function(){ playButton.html('3');	},200);
-		window.setTimeout(function(){ playButton.html('2');	},700);
-		window.setTimeout(function(){ playButton.html('1');	},1200);
-		window.setTimeout(function(){ playButton.hide(); },1700);
-		window.setTimeout(function(){
-			play = window.setInterval(function(){
+
+	window.setInterval(function(){
+			
+		pos_x1 = d_left == 1 ? pos_x1 + movimento_left : pos_x1 - movimento_left;
+		pos_y1 = d_top == 1 ? pos_y1 + movimento_top : pos_y1 - movimento_top;
 				
-				pos_x = d_left == 1 ? pos_x + movimento_left : pos_x - movimento_left;
-				pos_y = d_top == 1 ? pos_y + movimento_top : pos_y - movimento_top;
+		if(pos_x1<limite_left){
+			pos_x1 = pos_x1 + movimento_left * 2;
+			d_left = 1;
+		}
+			
+		if(pos_x1>limite_right){
+			pos_x1 = pos_x1 - movimento_left * 2;
+			d_left = 0;
+		}
 				
-				if(pos_x<limite_left){
-					pos_x = pos_x + movimento_left * 2;
-					d_left = 1;
-				}
+		$('#zumbie_1').css({'top':50,'left':pos_x1+'px'});
+			
+	},33);
+	
+	window.setInterval(function(){
+			
+		pos_x2 = d_left == 1 ? pos_x2 + movimento_left : pos_x2 - movimento_left;
+		pos_y2 = d_top == 1 ? pos_y2 + movimento_top : pos_y2 - movimento_top;
 				
-				if(pos_x>limite_right){
-					pos_x = pos_x - movimento_left * 2;
-					d_left = 0;
-				}
+		if(pos_x2<limite_left){
+			pos_x2 = pos_x2 + movimento_left * 2;
+			d_left = 1;
+		}
+			
+		if(pos_x2>limite_right){
+			pos_x2 = pos_x2 - movimento_left * 2;
+			d_left = 0;
+		}
 				
-				if(pos_y<limite_top){
-					pos_y = pos_y + movimento_top * 2;
-					d_top = 1;
-				}
-				
-				if(pos_y>limite_bottom){
-					pos_x = pos_x + movimento_top * 2;
-					d_top = 0;
-				}
-				
-				$('#zumbie_1').css({'top':pos_y+'px','left':pos_x+'px'});
-				
-			},33);
-		},1900);
-	});
+		$('#zumbie_2').css({'top':150,'left':pos_x2+'px'});
+			
+	},50);
+	
 });
